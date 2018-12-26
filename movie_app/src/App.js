673a0f13a761d42ae8d7e5635d21c7baea088944
DaMacho <po4865@gmail.com>
2018-12-26 14:12:43 +0900

    change on App.js movies list added into App as an state. setState example add in App.

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -2,30 +2,53 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
-const movies = [
-  {
-    title: "Matrix",
-    poster: "https://upload.wikimedia.org/wikipedia/en/thumb/9/9a/The_Matrix_soundtrack_cover.jpg/220px-The_Matrix_soundtrack_cover.jpg"
-  },
-  {
-    title: "Full Metal Jacket",
-    poster: "https://upload.wikimedia.org/wikipedia/en/thumb/9/99/Full_Metal_Jacket_poster.jpg/220px-Full_Metal_Jacket_poster.jpg"
-  },
-  {
-    title: "Oldboy",
-    poster: "https://upload.wikimedia.org/wikipedia/en/thumb/b/bb/Oldboy_2013_film_poster.jpg/220px-Oldboy_2013_film_poster.jpg"
-  },
-  {
-    title: "Star Wars",
-    poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5qrrSKkr5IDPZZgSvoCG7XXm6DXJnWq6IfBMM8SxGjfCIdzx5uQ"
-  },
-]
-
 class App extends Component {
+  // Render: componentWillMount() -> render() -> componentDidMount()
+  // Update: componentWillReceiveProps() -> shouldComponentUpdate() (== true) -> componentWillUpdate() -> render() -> componentDidUpdate()
+
+  state = {
+    greeting: 'Hello!',
+    movies: [
+      {
+        title: "Matrix",
+        poster: "https://upload.wikimedia.org/wikipedia/en/thumb/9/9a/The_Matrix_soundtrack_cover.jpg/220px-The_Matrix_soundtrack_cover.jpg"
+      },
+      {
+        title: "Full Metal Jacket",
+        poster: "https://upload.wikimedia.org/wikipedia/en/thumb/9/99/Full_Metal_Jacket_poster.jpg/220px-Full_Metal_Jacket_poster.jpg"
+      },
+      {
+        title: "Oldboy",
+        poster: "https://upload.wikimedia.org/wikipedia/en/thumb/b/bb/Oldboy_2013_film_poster.jpg/220px-Oldboy_2013_film_poster.jpg"
+      },
+      {
+        title: "Star Wars",
+        poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5qrrSKkr5IDPZZgSvoCG7XXm6DXJnWq6IfBMM8SxGjfCIdzx5uQ"
+      },
+    ]
+  }
+
+  componentDidMount(){
+    setTimeout(() => {
+      this.setState({
+        movies: [
+          ...this.state.movies,
+          {
+            title: "Transformer",
+            poster: "https://upload.wikimedia.org/wikipedia/en/thumb/6/66/Transformers07.jpg/220px-Transformers07.jpg"
+          }
+          // ...this.state.movies,
+        ]
+      })  
+    }, 2000)
+  }
+
   render() {
+    // console.log('did render')
     return (
       <div className="App">
-        {movies.map((movie, index) => {
+        {this.state.greeting}
+        {this.state.movies.map((movie, index) => {
           return <Movie title={movie.title} poster={movie.poster} key={index} />
         })}
       </div>
