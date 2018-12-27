8ab613d2845ca8d429099e0b01db66f8ac47386e
DaMacho <po4865@gmail.com>
2018-12-27 13:21:43 +0900

    change on Movie.js changed state-base class components which are Movie and MoviePoster, into stateless functional components.

--- movie_app/src/Movie.js
+++ movie_app/src/Movie.js
@@ -1,7 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
 
+// The smart component have 'state', but dum component doesn't have 'state'
+// use them cautiously.
+
+// used 'stateless functional component' Cuz this component no need any state(no lifecyle). Only return prop.
+function Movie({title, poster}){
+  return(
+    <div>
+      <MoviePoster poster={poster} />
+      <h1>{title}</h1>
+    </div>
+  )
+}
+/*
 class Movie extends Component {
 
   static propTypes = {
@@ -19,7 +32,15 @@ class Movie extends Component {
     )
   }
 }
+*/
 
+
+function MoviePoster({poster}){
+  return (
+    <img src={poster} alt="Movie Poster" />
+  )
+}
+/*
 class MoviePoster extends Component {
 
   static propTypes = {
@@ -31,6 +52,17 @@ class MoviePoster extends Component {
       <img src={this.props.poster} alt="Movie Poster" />
     )
   }
+}
+*/
+
+
+// PropTypes check
+Movie.propTypes = {
+  title: PropTypes.string.isRequired,
+  poster: PropTypes.string.isRequired
+}
+MoviePoster.propTypes = {
+  poster: PropTypes.string.isRequired
 }
 
 export default Movie;
