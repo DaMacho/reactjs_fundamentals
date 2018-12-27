f47669eaaef577951a2461f9d4e0faa998711575
DaMacho <po4865@gmail.com>
2018-12-27 21:05:11 +0900

    putted fetch() in App.js to handle data with json form.

--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -11,34 +11,10 @@ class App extends Component {
   }
 
   componentDidMount(){
-    setTimeout(() => {
-      this.setState({
-          // ...this.state.movies,
-          movies: [
-            {
-              title: "Matrix",
-              poster: "https://upload.wikimedia.org/wikipedia/en/thumb/9/9a/The_Matrix_soundtrack_cover.jpg/220px-The_Matrix_soundtrack_cover.jpg"
-            },
-            {
-              title: "Full Metal Jacket",
-              poster: "https://upload.wikimedia.org/wikipedia/en/thumb/9/99/Full_Metal_Jacket_poster.jpg/220px-Full_Metal_Jacket_poster.jpg"
-            },
-            {
-              title: "Oldboy",
-              poster: "https://upload.wikimedia.org/wikipedia/en/thumb/b/bb/Oldboy_2013_film_poster.jpg/220px-Oldboy_2013_film_poster.jpg"
-            },
-            {
-              title: "Star Wars",
-              poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5qrrSKkr5IDPZZgSvoCG7XXm6DXJnWq6IfBMM8SxGjfCIdzx5uQ"
-            },
-            {
-              title: "Transformer",
-              poster: "https://upload.wikimedia.org/wikipedia/en/thumb/6/66/Transformers07.jpg/220px-Transformers07.jpg"
-            },
-          ]
-          // ...this.state.movies,
-      })  
-    }, 2000)
+    fetch('https://yts.am/api/v2/list_movies.json?sort_by=download_count')
+    .then(response => response.json())
+    .then(json => console.log(json))
+    .catch(err => console.log(err))
   }
 
   _renderMovies = () => {
@@ -59,3 +35,34 @@ class App extends Component {
 }
 
 export default App;
+
+/*
+setTimeout(() => {
+  this.setState({
+      // ...this.state.movies,
+      movies: [
+        {
+          title: "Matrix",
+          poster: "https://upload.wikimedia.org/wikipedia/en/thumb/9/9a/The_Matrix_soundtrack_cover.jpg/220px-The_Matrix_soundtrack_cover.jpg"
+        },
+        {
+          title: "Full Metal Jacket",
+          poster: "https://upload.wikimedia.org/wikipedia/en/thumb/9/99/Full_Metal_Jacket_poster.jpg/220px-Full_Metal_Jacket_poster.jpg"
+        },
+        {
+          title: "Oldboy",
+          poster: "https://upload.wikimedia.org/wikipedia/en/thumb/b/bb/Oldboy_2013_film_poster.jpg/220px-Oldboy_2013_film_poster.jpg"
+        },
+        {
+          title: "Star Wars",
+          poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5qrrSKkr5IDPZZgSvoCG7XXm6DXJnWq6IfBMM8SxGjfCIdzx5uQ"
+        },
+        {
+          title: "Transformer",
+          poster: "https://upload.wikimedia.org/wikipedia/en/thumb/6/66/Transformers07.jpg/220px-Transformers07.jpg"
+        },
+      ]
+      // ...this.state.movies,
+  })  
+}, 2000)
+*/
