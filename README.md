c2f05ae2991411f5d4a0f25ea1e354a5c8dbff0e
DaMacho <po4865@gmail.com>
2018-12-27 12:48:13 +0900

    implemented loading states edited App.js that implemented loading states to delay and show data.

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # reactjs_fundamentals
 
-expecting result from [**movie_app**](https://dribbble.com/shots/2442798-Movie-Application)
+expect result to be like the [**movie_app**](https://dribbble.com/shots/2442798-Movie-Application)
--- movie_app/src/App.js
+++ movie_app/src/App.js
@@ -7,50 +7,52 @@ class App extends Component {
   // Update: componentWillReceiveProps() -> shouldComponentUpdate() (== true) -> componentWillUpdate() -> render() -> componentDidUpdate()
 
   state = {
-    greeting: 'Hello!',
-    movies: [
-      {
-        title: "Matrix",
-        poster: "https://upload.wikimedia.org/wikipedia/en/thumb/9/9a/The_Matrix_soundtrack_cover.jpg/220px-The_Matrix_soundtrack_cover.jpg"
-      },
-      {
-        title: "Full Metal Jacket",
-        poster: "https://upload.wikimedia.org/wikipedia/en/thumb/9/99/Full_Metal_Jacket_poster.jpg/220px-Full_Metal_Jacket_poster.jpg"
-      },
-      {
-        title: "Oldboy",
-        poster: "https://upload.wikimedia.org/wikipedia/en/thumb/b/bb/Oldboy_2013_film_poster.jpg/220px-Oldboy_2013_film_poster.jpg"
-      },
-      {
-        title: "Star Wars",
-        poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5qrrSKkr5IDPZZgSvoCG7XXm6DXJnWq6IfBMM8SxGjfCIdzx5uQ"
-      },
-    ]
+
   }
 
   componentDidMount(){
     setTimeout(() => {
       this.setState({
-        movies: [
-          ...this.state.movies,
-          {
-            title: "Transformer",
-            poster: "https://upload.wikimedia.org/wikipedia/en/thumb/6/66/Transformers07.jpg/220px-Transformers07.jpg"
-          }
           // ...this.state.movies,
-        ]
+          movies: [
+            {
+              title: "Matrix",
+              poster: "https://upload.wikimedia.org/wikipedia/en/thumb/9/9a/The_Matrix_soundtrack_cover.jpg/220px-The_Matrix_soundtrack_cover.jpg"
+            },
+            {
+              title: "Full Metal Jacket",
+              poster: "https://upload.wikimedia.org/wikipedia/en/thumb/9/99/Full_Metal_Jacket_poster.jpg/220px-Full_Metal_Jacket_poster.jpg"
+            },
+            {
+              title: "Oldboy",
+              poster: "https://upload.wikimedia.org/wikipedia/en/thumb/b/bb/Oldboy_2013_film_poster.jpg/220px-Oldboy_2013_film_poster.jpg"
+            },
+            {
+              title: "Star Wars",
+              poster: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ5qrrSKkr5IDPZZgSvoCG7XXm6DXJnWq6IfBMM8SxGjfCIdzx5uQ"
+            },
+            {
+              title: "Transformer",
+              poster: "https://upload.wikimedia.org/wikipedia/en/thumb/6/66/Transformers07.jpg/220px-Transformers07.jpg"
+            },
+          ]
+          // ...this.state.movies,
       })  
     }, 2000)
+  }
+
+  _renderMovies = () => {
+    const movies = this.state.movies.map((movie, index) => {
+      return <Movie title={movie.title} poster={movie.poster} key={index} />
+    })
+    return movies
   }
 
   render() {
     // console.log('did render')
     return (
       <div className="App">
-        {this.state.greeting}
-        {this.state.movies.map((movie, index) => {
-          return <Movie title={movie.title} poster={movie.poster} key={index} />
-        })}
+        {this.state.movies ? this._renderMovies() : 'Loading'}
       </div>
     );
   }
