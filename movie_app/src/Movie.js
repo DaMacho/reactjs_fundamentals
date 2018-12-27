6ffa7d29abcbb7dd21c06f0a27335152e65dd489
DaMacho <po4865@gmail.com>
2018-12-27 22:00:38 +0900

    add more Props edited App.js to get more Info frem date, and edited Movie.js to have more Props also registered Props. title, poster, genres, synopsis.

--- movie_app/src/Movie.js
+++ movie_app/src/Movie.js
@@ -6,11 +6,22 @@ import './Movie.css';
 // use them cautiously.
 
 // used 'stateless functional component' Cuz this component no need any state(no lifecyle). Only return prop.
-function Movie({title, poster}){
+function Movie({title, poster, genres, synopsis}){
   return(
-    <div>
-      <MoviePoster poster={poster} />
-      <h1>{title}</h1>
+    <div className="Movie">
+      <div className="Movie__Columns">
+        <MoviePoster poster={poster} alt={title} />
+      </div>
+      <div className="Movie__Columns">
+        <h1>{title}</h1>
+        <div className="Movie__Genres">
+          {genres.map((genre, index) => <MovieGenre genre={genre} key={index} />)}
+        </div>
+        <p className="Movie__Synopsis">
+          {synopsis}
+        </p>
+      </div>
+      
     </div>
   )
 }
@@ -35,9 +46,9 @@ class Movie extends Component {
 */
 
 
-function MoviePoster({poster}){
+function MoviePoster({poster, alt}){
   return (
-    <img src={poster} alt="Movie Poster" />
+    <img src={poster} alt={alt} title={alt} className="Movie__Poster" />
   )
 }
 /*
@@ -56,13 +67,27 @@ class MoviePoster extends Component {
 */
 
 
+function MovieGenre({genre}){
+  return (
+    <span className="Movie__Genre">{genre}</span>
+  )
+}
+
+
 // PropTypes check
 Movie.propTypes = {
   title: PropTypes.string.isRequired,
-  poster: PropTypes.string.isRequired
+  poster: PropTypes.string.isRequired,
+  genres: PropTypes.array.isRequired,
+  synopsis: PropTypes.string.isRequired,
 }
 MoviePoster.propTypes = {
-  poster: PropTypes.string.isRequired
+  poster: PropTypes.string.isRequired,
+  alt: PropTypes.string.isRequired
+}
+
+MovieGenre.propTypes = {
+  genres: PropTypes.string.isRequired
 }
 
 export default Movie;
